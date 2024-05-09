8a2c75f7a4a2556de9c1fc7a97ec8d9ac541345c
Mark V <markromanv@gmail.com>
2024-05-08 23:14:11 -0700

    fix GET

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export default function GET(req:NextRequest){
+export async function GET(req:NextRequest){
     return new NextResponse("Hello World", {status:200})
 }
